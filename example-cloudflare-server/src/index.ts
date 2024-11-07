f0b6b9831b7688b350bd3cd1272fa7fc4b3d6730
Aksh4code <akkisingh8266@gmail.com>
2024-11-08 02:43:05 +0530

    created basic route

--- example-cloudflare-server/src/index.ts
+++ example-cloudflare-server/src/index.ts
@@ -1,18 +1,18 @@
-/**
- * Welcome to Cloudflare Workers! This is your first worker.
- *
- * - Run `npm run dev` in your terminal to start a development server
- * - Open a browser tab at http://localhost:8787/ to see your worker in action
- * - Run `npm run deploy` to publish your worker
- *
- * Bind resources to your worker in `wrangler.toml`. After adding bindings, a type definition for the
- * `Env` object can be regenerated with `npm run cf-typegen`.
- *
- * Learn more at https://developers.cloudflare.com/workers/
- */
+import { Hono } from "hono";
+import { Bindings } from "hono/types";
 
-export default {
-	async fetch(request, env, ctx): Promise<Response> {
-		return new Response('Hello World!');
-	},
-} satisfies ExportedHandler<Env>;
+export type Env = {
+	DATABASE_URL : string
+}
+
+const app = new Hono<{Bindings : Env}>()
+
+app.get('/', async (c) => {
+	console.log(c.env.DATABASE_URL);
+	
+	const body = await c.req.parseBody()
+	console.log(body);
+	return c.json({msg: "hey There"})
+  })
+
+  export default app
